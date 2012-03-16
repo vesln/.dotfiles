f9a3c981520d30cecc469a1c36f08b9473452fe5
Veselin Todorov <veselin@veselin.bg>
2012-03-16 18:12:45 +0200

    Ruby tweaks.

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,7 +1,10 @@
+alias ree="RUBY_HEAP_MIN_SLOTS=1000000 RUBY_HEAP_SLOTS_INCREMENT=1000000 RUBY_HEAP_SLOTS_GROWTH_FACTOR=1 RUBY_GC_MALLOC_LIMIT=1000000000 RUBY_HEAP_FREE_MIN=500000"
+
 alias b="bundle"
 alias be="bundle exec"
 
-alias cuke="clear && cucumber"
-alias spec="clear && rspec"
+alias cuke="clear && be cucumber"
+alias spec="clear && be rspec"
 
 export CUCUMBER_FORMAT='progress'
+
