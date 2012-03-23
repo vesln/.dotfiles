7bb86abb3a3d3d4f7196a1b89c4348b222f7904d
Veselin Todorov <veselin@veselin.bg>
2012-03-23 22:20:51 +0200

    Update git aliases

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,6 +1,9 @@
 # Pretty git log.
 alias gl="git log --graph --pretty=format':%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset'"
 
+# Git pull
+alias gpr="git pull --rebase"
+
 # Allows you to skip the quotes after git commit -m.
 # Thanks to @skanev
 function gci () {
