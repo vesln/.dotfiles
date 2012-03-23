bcff138270784b7d405495090578af8ad7a75ecb
Veselin Todorov <veselin@veselin.bg>
2012-03-23 22:02:52 +0200

    Add convenient git commit function

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,2 +1,8 @@
 # Pretty git log.
 alias gl="git log --graph --pretty=format':%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset'"
+
+# Allows you to skip the quotes after git commit -m.
+# Thanks to @skanev
+function gci () {
+  git commit -m "$*"
+}
