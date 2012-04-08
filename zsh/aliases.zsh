1641ab1975f174eaff5f6493289da1efaa317ebc
Veselin Todorov <veselin@veselin.bg>
2012-04-08 12:17:43 +0300

    Better aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -33,16 +33,18 @@ alias gst="git status"
 alias gs="git stash"
 alias gsp="git stash pop"
 
+alias gco="git checkout"
+
 ## Ruby
 alias ree="RUBY_HEAP_MIN_SLOTS=1000000 RUBY_HEAP_SLOTS_INCREMENT=1000000 RUBY_HEAP_SLOTS_GROWTH_FACTOR=1 RUBY_GC_MALLOC_LIMIT=1000000000 RUBY_HEAP_FREE_MIN=500000"
 
 alias b="bundle"
 alias be="bundle exec"
 
-alias cuke="clear && ree be cucumber --drb"
-alias spec="clear && ree be rspec --drb"
+alias cuke="clear && be cucumber --drb"
+alias spec="clear && be rspec --drb"
 
-alias spj="ree be specjour --workers 2"
+alias spj="be specjour --workers 2"
 
 alias rdm="rake db:migrate"
 alias rdmr="rake db:migrate:reset"
