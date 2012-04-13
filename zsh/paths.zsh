012bdf346d72149e810539f2d2539cc042fc0085
Veselin Todorov <veselin@veselin.bg>
2012-04-13 16:31:23 +0300

    Remove duplicated path

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -2,6 +2,5 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin
 
 ## rvm
-export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
 PATH=$PATH:$HOME/.rvm/bin
