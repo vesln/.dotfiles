782475df36bd602bc01200988f439d747757600b
Veselin Todorov <veselin@veselin.bg>
2012-07-22 15:33:35 +0300

    Remove ree

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -39,7 +39,6 @@ alias gco="git checkout"
 alias gsf="git submodule foreach git pull origin master"
 
 # Ruby
-alias ree="RUBY_HEAP_MIN_SLOTS=1000000 RUBY_HEAP_SLOTS_INCREMENT=1000000 RUBY_HEAP_SLOTS_GROWTH_FACTOR=1 RUBY_GC_MALLOC_LIMIT=1000000000 RUBY_HEAP_FREE_MIN=500000"
 alias b="bundle"
 alias be="bundle exec"
 
@@ -53,8 +52,8 @@ alias rdr="rake db:rollback"
 
 alias rgm="rails g migration"
 
-alias spr="ree be spork rspec"
-alias spc="ree be spork cucumber"
+alias spr="be spork rspec"
+alias spc="be spork cucumber"
 
 # Node.js
 alias t="clear && make test"
