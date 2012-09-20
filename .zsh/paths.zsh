6f88fb4157c40a148508d7f2066269441372b16a
Veselin Todorov <veselin@veselin.bg>
2012-09-20 11:30:03 +0300

    Pythonbrew + virtualenvwrapper

--- .zsh/paths.zsh
+++ .zsh/paths.zsh
@@ -8,12 +8,18 @@ PATH=$PATH:$HOME/.rvm/bin
 # tmuxinator
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
 
+# pythonbrew
+[[ -s $HOME/.pythonbrew/etc/bashrc ]] && source $HOME/.pythonbrew/etc/bashrc
+
 # VirtualEnv
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
 export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 export PIP_RESPECT_VIRTUALENV=true
+
+# VirtualEnvWrapper
+source /usr/local/share/python/virtualenvwrapper.sh
 
 # Load nvm
 . ~/.nvm/nvm.sh
