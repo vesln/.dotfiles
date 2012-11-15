85fabec22cc5a8a5e4f3c69107283e7c6982f458
Veselin Todorov <veselin@veselin.bg>
2012-11-15 16:10:15 +0200

    I hate history...

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -30,3 +30,17 @@ setopt always_to_end
 
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:*:*:*' menu select
+
+# Command history
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt append_history
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt share_history
