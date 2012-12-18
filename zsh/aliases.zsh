ee2a4d76981899356b547dee55c5c18f268b7e23
Veselin Todorov <veselin@veselin.bg>
2012-12-18 08:31:35 +0200

    Git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,12 +8,14 @@ alias gb="git branch"
 alias gbd="git branch -D"
 
 alias gl="git log --graph --pretty=format':%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset'"
+alias gla="gl --all"
 alias gpr="git pull --rebase"
 
 alias gp="git push"
 alias gpu="git-push-with-tracking"
 
 alias ga="git add"
+alias gap="git add -p"
 alias gaa="git add -A"
 
 alias gc="git commit"
@@ -29,18 +31,21 @@ alias grs="git reset HEAD\^ --soft"
 alias gcf="git clean -f"
 alias gcd="git clean -d"
 
-alias gs="git stash"
-alias gsp="git stash pop"
+alias gsh="git stash"
+alias gshp="git stash pop"
 
 alias gd="git diff"
 alias gdc="git diff --cached"
 
-alias gst="git status"
+alias gst="git status --short"
 alias gco="git checkout"
 
 alias gsf="git submodule foreach git pull origin master"
 
 alias gdm="git delete-merged"
+
+alias gm='git merge --no-ff'
+alias gmf='git merge --ff-only'
 
 # Rails
 alias b="bundle"
