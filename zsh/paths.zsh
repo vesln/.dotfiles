e025f4fc37eec6e72fcb38f3a3e94737d4093563
Veselin Todorov <veselin@veselin.bg>
2014-01-10 15:29:17 +0200

    Path cleanup

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -2,13 +2,13 @@
 #PATH=$PATH:$HOME/.rvm/bin
 
 # Default
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:~/.bin/:/usr/local/share/python
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:~/.bin
 
 # tmuxinator
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
 
 # pythonbrew
-[[ -s "$HOME/.pythonbrew/etc/bashrc" ]] && source "$HOME/.pythonbrew/etc/bashrc"
+#[[ -s "$HOME/.pythonbrew/etc/bashrc" ]] && source "$HOME/.pythonbrew/etc/bashrc"
 
 # Load nvm
 . ~/.nvm/nvm.sh
