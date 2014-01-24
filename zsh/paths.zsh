9c33a0ba0aefa672224645fae44c277d08e7ea36
Veselin Todorov <veselin@veselin.bg>
2014-01-24 13:43:21 +0200

    Go

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -2,7 +2,7 @@
 #PATH=$PATH:$HOME/.rvm/bin
 
 # Default
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:~/.bin
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:~/.bin:/usr/local/go/bin
 
 # tmuxinator
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
