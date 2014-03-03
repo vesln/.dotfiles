e2baa3e3bcb4a07bd1bc3181b52624995f11b130
Veselin Todorov <veselin@veselin.bg>
2014-03-03 12:22:41 +0200

    Remove pythonbrew

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -7,8 +7,5 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X1
 # tmuxinator
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
 
-# pythonbrew
-#[[ -s "$HOME/.pythonbrew/etc/bashrc" ]] && source "$HOME/.pythonbrew/etc/bashrc"
-
 # Load nvm
 . ~/.nvm/nvm.sh
