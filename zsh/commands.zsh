8b53bf75557cf6502fcbb9c77fbb32f9820eef07
Veselin Todorov <veselin@veselin.bg>
2014-03-08 22:58:32 +0100

    Remove unused commands

--- zsh/commands.zsh
+++ zsh/commands.zsh
@@ -1,21 +1,11 @@
 # Commit without the quotes
+
 gci() {
   git commit -m "$*"
 }
 
 # Push and set tracking
+
 git-push-with-tracking() {
   git push -u origin $(git branch | grep "*" | sed "s/* //")
-}
-
-# Build & publish a private gem
-fpush() {
-  gem build `ls *.gemspec`
-  fury push `ls *.gem`
-  rm `ls *.gem`
-}
-
-# Kill spork
-ss() {
-  pgrep -f spork | xargs kill -9
 }
