c8d819472e73b02879e2372f8388551f8dcbe200
Veselin Todorov <veselin@veselin.bg>
2014-03-26 22:25:41 +0100

    Alias cleanup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,7 @@ alias l="ls -lahF"
 
 # Git
 
+alias gb="git branch"
 alias gbd="git branch -D"
 
 alias gl="git log-pretty"
@@ -14,21 +15,14 @@ alias gp="git push"
 alias gpu="git-push-with-tracking"
 
 alias ga="git add"
-alias gap="git add -p"
 alias gaa="git add -A"
 
 alias gc="git commit -v"
-alias gcam="git commit -v --amend"
 alias gcm="git commit -m"
 
 alias gr="git rebase"
 alias gra="git rebase --abort"
 alias grc="git rebase --continue"
-
-alias grs="git reset HEAD\^ --soft"
-
-alias gsh="git stash"
-alias gshp="git stash pop"
 
 alias gd="git diff"
 alias gdc="git diff --cached"
@@ -46,9 +40,8 @@ alias gmf='git merge --ff-only'
 # Ruby / Rails
 
 alias be="bundle exec"
-alias rdm="bundle exec rake db:migrate"
-alias rdmr="bundle exec rake db:migrate:reset"
-alias rdtp="bundle exec rake db:test:prepare"
+alias rdm="bin/rake db:migrate"
+alias rdtp="bin/rake db:test:prepare"
 
 alias cuke="be cucumber --drb"
 alias spec="be rspec spec/ --drb"
