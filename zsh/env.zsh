7ff7f32e56d95e1278e216c91bfd993a61dba96d
Veselin Todorov <veselin@veselin.bg>
2014-04-12 10:49:37 +0300

    Add elasticbeanstalk

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,2 +1,6 @@
 # Editor
 export EDITOR="vim"
+
+# Beanstalk
+export AWS_CREDENTIAL_FILE=~/.eb_aws_cred
+export ELASTICBEANSTALK_URL="https://elasticbeanstalk.us-east-1.amazonaws.com"
--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,1 +1,1 @@
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:~/.bin:/usr/local/go/bin
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:~/.bin:/usr/local/go/bin:~/.eb/api/bin:~/.eb/eb/macosx/python2.7
