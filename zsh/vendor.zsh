557a4438b8425135eb03759e338539488f18f255
Veselin Todorov <veselin@veselin.bg>
2015-04-29 09:09:27 +0300

    Nuke nvm

--- zsh/vendor.zsh
+++ zsh/vendor.zsh
@@ -1,9 +1,6 @@
 # tmuxinator
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
 
-# Load nvm
-. ~/.nvm/nvm.sh
-
 # Load Travis
 [ -f ~/.travis/travis.sh ] && source ~/.travis/travis.sh
 
