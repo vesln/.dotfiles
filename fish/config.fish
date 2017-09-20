a10fdb8e1cb7168ccb1d8852440a58a8ac12aa91
Veselin Todorov <veselin@veselin.bg>
2017-09-20 16:20:58 +0300

    Nuke desk

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,3 @@
 . "$HOME/.config/fish/init/aliases.fish"
 . "$HOME/.config/fish/init/export.fish"
 . "$HOME/.config/fish/init/local.fish"
-
-# Hook for desk activation
-test -n "$DESK_ENV"; and . "$DESK_ENV"; or true
