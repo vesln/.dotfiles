ac95be48ef6cc2d4bde6be7204b13055b71efed1
Veselin Todorov <veselin@veselin.bg>
2018-09-04 14:29:22 +0300

    Fix typo

--- fish/functions/grh.fish
+++ fish/functions/grh.fish
@@ -2,7 +2,7 @@ function grh
   echo 'git reset & clean...'
 
   if read_confirm
-    git reset head --hard > /dev/null
+    git reset HEAD --hard > /dev/null
     git clean -fd > /dev/null
   end
 end
